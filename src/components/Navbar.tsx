3c1518401c74c8a0767cfb5468f1e5a63aa2f534
heee1 <p1xell@kakao.com>
2024-10-21 22:31:17 +0900

    [feat] navbar구현 및 공통컴포넌트로 추가
    - navbar를 구현하였습니다.
    - todo-list todo-detail 페이지에 공통적으로 보여져야하기 때문에
      layout.tsx파일에 추가하여 처리하였습니다.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import navbarLogoImage from "../assets/image/navbar-logo.png";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* NAVBAR CONTAINER */}
+      <div className="flex pl-[30px] lg:pl-[360px] items-center w-full h-[60px] border-b-2">
+        <Link href={"/"}>
+          <Image src={navbarLogoImage} alt="navbar-logo" />
+        </Link>
+      </div>
+    </>
+  );
+};
+export default Navbar;
