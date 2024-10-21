e3a2691e1fe7d68b5b23bc0aa539d0276d2d9fb5
heee1 <p1xell@kakao.com>
2024-10-21 22:34:10 +0900

    [feat] todo-list, todo-detail 페이지 및 기능 구현
    - todo들의 목록이 보여지는 list페이지와
      todo의 상세내용을 확인할 수 있는 detail페이지를 구현하였습니다.
    - todo-list의 각 기능을 구현하였으며 상세기능은 다음과 같습니다.
      - todo 추가
      - todo 토글
      - todo 삭제
      - todo 수정
      - todo의 id를 활용한 상세페이지 라우팅
      - todo memo 작성/수정
    - 이미지 업로드 기능은 미구현 상태입니다.

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import checkButtonTrue from "../assets/image/check-button-true.png";
+import checkButtonFalse from "../assets/image/check-button-false.png";
+import emptyTodo from "../assets/image/empty_todo.png";
+import emptyDone from "../assets/image/empty_done.png";
+import { TTodo } from "@/app/types/types";
+import api from "@/app/api/api";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState<TTodo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await api.get(`/items`);
+      setTodos(response.data);
+    };
+
+    fetchTodos();
+  }, []);
+
+  const addTodoHandler = async () => {
+    if (!newTodo.trim()) return;
+
+    const response = await api.post(`/items`, {
+      name: newTodo,
+    });
+
+    setTodos([...todos, response.data]);
+    setNewTodo("");
+  };
+
+  const toggleTodoHandler = async (id: string) => {
+    const targetTodo = todos.find((todo) => todo.id === id);
+    if (!targetTodo) return;
+
+    const updatedIsCompleted = !targetTodo.isCompleted;
+
+    await api.patch(`/items/${id}`, {
+      isCompleted: !targetTodo.isCompleted,
+    });
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: updatedIsCompleted } : todo
+      )
+    );
+  };
+
+  return (
+    <>
+      <div className="flex-1 flex flex-col items-center px-4 md:px-16 lg:px-72 mt-12">
+        {/* INPUT AREA */}
+        <div className="flex flex-col md:flex-col lg:flex-row justify-center w-full h-14">
+          <input
+            type="text"
+            placeholder="할 일을 입력해주세요"
+            className="w-full md:w-3/4 h-full pl-8 border-2 bg-slate-100 border-black rounded-full drop-shadow-[5px_5px_0px_rgba(0,0,0,0.8)] font-bold"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button
+            className="w-full md:w-1/4 h-full mt-4 md:mt-0 md:ml-8 border-2 bg-slate-100 border-black rounded-full drop-shadow-[5px_5px_0px_rgba(0,0,0,0.8)] shadow-lg font-bold"
+            onClick={() => addTodoHandler()}
+          >
+            + 추가하기
+          </button>
+        </div>
+
+        {/* LIST AREA */}
+        <div className="flex flex-col md:flex-row w-full mt-12 gap-8">
+          {/* TODO LIST AREA */}
+          <div className="w-full md:w-1/2">
+            <div className="w-24 h-9 text-center leading-9 bg-lime-300 text-green-700 font-bold text-lg rounded-full">
+              TO DO
+            </div>
+            <div className="flex flex-col gap-4 mt-3 ">
+              {todos.filter((todo) => !todo.isCompleted).length > 0 ? (
+                todos
+                  .filter((todo) => !todo.isCompleted)
+                  .map((todo) => (
+                    <Link href={`/items/${todo.id}`}>
+                      <div
+                        key={todo.id}
+                        className="w-full h-12 pl-4 flex flex-row rounded-full items-center justify-start gap-5 border-black border-2 bg-white"
+                      >
+                        <Image
+                          src={
+                            todo.isCompleted
+                              ? checkButtonTrue
+                              : checkButtonFalse
+                          }
+                          alt={
+                            todo.isCompleted
+                              ? "check-button-true"
+                              : "check-button-false"
+                          }
+                          className="w-8 h-8 cursor-pointer"
+                          onClick={() => toggleTodoHandler(todo.id)}
+                        />
+                        {todo.name}
+                      </div>
+                    </Link>
+                  ))
+              ) : (
+                <>
+                  <div className="flex flex-col items-center font-bold text-slate-400">
+                    <Image src={emptyTodo} alt="empty-todo" />
+                    <span>할 일이 없어요.</span>
+                    <span>TODO를 새롭게 추가해주세요!</span>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+
+          {/* DONE LIST AREA */}
+          <div className="w-full md:w-1/2">
+            <div className="w-24 h-9 text-center leading-9 bg-green-700 text-amber-300 font-bold text-lg rounded-full">
+              DONE
+            </div>
+            <div className="flex flex-col gap-4 mt-3 ">
+              {todos.filter((todo) => todo.isCompleted).length > 0 ? (
+                todos
+                  .filter((todo) => todo.isCompleted)
+                  .map((todo) => (
+                    <div
+                      key={todo.id}
+                      className="w-full h-12 pl-4 flex flex-row rounded-full items-center justify-start gap-5 border-black border-2 bg-violet-100"
+                    >
+                      <Image
+                        src={
+                          todo.isCompleted ? checkButtonTrue : checkButtonFalse
+                        }
+                        alt={
+                          todo.isCompleted
+                            ? "check-button-true"
+                            : "check-button-false"
+                        }
+                        className="w-8 h-8 cursor-pointer"
+                        onClick={() => toggleTodoHandler(todo.id)}
+                      />
+                      <span className="line-through">{todo.name}</span>
+                    </div>
+                  ))
+              ) : (
+                <>
+                  <div className="flex flex-col items-center font-bold text-slate-400">
+                    <Image src={emptyDone} alt="empty-done" />
+                    <span>아직 다 한 일이 없어요.</span>
+                    <span>해야 할 일을 체크해보세요!</span>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default TodoList;
